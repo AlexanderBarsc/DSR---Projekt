85ff589ab514427ffe196826365fbe2d1e57270d
AlexanderBarsc <82763122+AlexanderBarsc@users.noreply.github.com>
2022-12-17 16:49:07 +0100

    WIP.

---
+++ DSŘ/DSŘ/Main.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace DSŘ
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.IPAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ErrorTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(154, 32);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // IPAddress
+            // 
+            this.IPAddress.Location = new System.Drawing.Point(25, 35);
+            this.IPAddress.Name = "IPAddress";
+            this.IPAddress.Size = new System.Drawing.Size(100, 20);
+            this.IPAddress.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Address";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ErrorTextBox
+            // 
+            this.ErrorTextBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ErrorTextBox.Location = new System.Drawing.Point(0, 430);
+            this.ErrorTextBox.Name = "ErrorTextBox";
+            this.ErrorTextBox.ReadOnly = true;
+            this.ErrorTextBox.Size = new System.Drawing.Size(800, 20);
+            this.ErrorTextBox.TabIndex = 5;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ErrorTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPAddress);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "Main";
+            this.Text = "DSR - Projekt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox IPAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox ErrorTextBox;
+    }
+}
+
---
+++ DSŘ/DSŘ/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Sharp7;
+
+namespace DSŘ
+{
+    public partial class Main : Form
+    {
+        private S7Client Client;
+        public Main()
+        {
+            Client = new S7Client();
+            InitializeComponent();
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateGui();
+        }
+
+        private void UpdateGui()
+        {
+            Client.MBRead()
+        }
+
+        /// <summary>
+        /// Handles connection to PLC.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            int result = 0; //TODO add better error handling
+            if(!Client.Connected)
+            {
+                result = Client.ConnectTo(IPAddress.Text, 0, 1);
+
+                if(result != 0)
+                {
+                    ErrorTextBox.Text = "Something went wrong during connection";
+                    return;
+                }
+                ConnectButton.Text = "Disconnect";
+            }
+            else
+            {
+                result = Client.Disconnect();
+
+                if (result != 0)
+                {
+                    ErrorTextBox.Text = "Something went wrong during disconnection";
+                    return;
+                }
+                ConnectButton.Text = "Connect";
+            }
+        }
+    }
+}
